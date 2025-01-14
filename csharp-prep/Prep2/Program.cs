8b9d70229f8dc8b154aca4bbabf4bd027900db78
Jason Gibson <jagibson@melaleuca.com>
2025-01-13 19:40:22 -0700

    programs 1 and 2 are finished

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage? ");
+        string gradeStr = Console.ReadLine();
+        int grade = int.Parse(gradeStr);
+        string letter = "F";
+        string message = "Congratulations, You've Passed the Class";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+            message = "Better Luck Next Time!";
+        }
+        else
+        {
+            letter = "F";
+            message = "Try Again, You Can Do It!";
+        }
+
+        Console.WriteLine($"Your Grade is a {letter}. {message}");
     }
 }
