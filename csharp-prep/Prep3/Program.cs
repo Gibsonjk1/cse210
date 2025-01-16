70c0546b50a53651f7634c7c911ddb1fb26ee2f8
Jason Gibson <jagibson@melaleuca.com>
2025-01-15 18:51:43 -0700

    project 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        bool again = true;
+
+        while (again != false)
+        {
+            Random random = new Random();
+            int magic = random.Next(1, 10);
+            Console.WriteLine("Guess the magic number? ");
+            bool guessed = false;
+
+
+            do
+            {
+                Console.WriteLine("What is your guess? ");
+                int guess = int.Parse(Console.ReadLine());
+
+                if (guess < magic)
+                {
+                    Console.WriteLine("Higher... ");
+                }
+                else if (guess > magic)
+                {
+                    Console.WriteLine("Lower... ");
+                }
+                else if (guess == magic)
+                {
+                    Console.WriteLine("You Guessed It!");
+                    guessed = true;
+                }
+            } while (guessed != true);
+
+            Console.WriteLine("Want to play again? (Y/N)");
+            string playAgain = Console.ReadLine();
+            if (playAgain.ToLower() == "n")
+            {
+                again = false;
+            }
+
+        }
     }
 }
