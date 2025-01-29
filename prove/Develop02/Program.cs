888eb039109cb54179816b7bb966fd91b5613f19
Jason Gibson <jagibson@melaleuca.com>
2025-01-28 18:46:51 -0700

    starting Journal

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Console.WriteLine("Hello! Welcome to Rockstar Journal!");
+        static void Menu()
+        {
+            bool stop = false;
+            while (stop == false)
+            {
+                Console.WriteLine("Please Choose an option: ");
+                Console.WriteLine("");
+                Console.WriteLine("1. Write a New Journal Entry");
+                Console.WriteLine("2. Display Your Journal");
+                Console.WriteLine("3. Save Your Journal to a File");
+                Console.WriteLine("4. Load Journal From a File");
+                Console.WriteLine("5. Quit");
+            }
+        }
+
     }
 }
